36472d1138ccf4e02dbba6a3ed0f032dfd3c142e
ravenstorm43 <201912065@office.induk.ac.kr>
2023-06-13 20:45:17 +0900

    2023-06-13 20:44

--- src/main/java/idusw/springboot/service/BoardServiceImpl.java
+++ src/main/java/idusw/springboot/service/BoardServiceImpl.java
@@ -50,7 +50,7 @@ public class BoardServiceImpl implements BoardService {
                 pageRequestDTO.getPageable(Sort.by("bno").descending()));
         Function<Object[], Board> fn = (entity -> entityToDto((BoardEntity) entity[0],
                 (MemberEntity) entity[1], (Long) entity[2]));
-        return new PageResultDTO<>(result, fn, 5);
+        return new PageResultDTO<>(result, fn, 8);
     }
 
 
